27b477d3f9444619be631cd7177cee65213932c6
iamareebjamal <jamal.areeb@gmail.com>
2017-10-08 15:46:29 +0530

    refac: Refactored folder structure

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask
+from apis import api
+from werkzeug.contrib.fixers import ProxyFix
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+api.init_app(app)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
